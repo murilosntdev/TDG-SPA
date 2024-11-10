dae0098f8e92776bbdfb59a1fc272bb3de8e803f
murilosntdev <murilosntdev@gmail.com>
2024-11-10 20:49:35 -0300

    theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { GlobalStyled } from "./GlobalStyled";
+import { ThemeProvider } from "styled-components";
+import theme from "./themes/theme";
 
 function App() {
   return (
     <>
       <GlobalStyled />
+      <ThemeProvider theme={theme}>
+      </ThemeProvider>
     </>
   );
 };
---
+++ src/themes/theme.js
@@ -0,0 +1,22 @@
+const theme = {
+    colors: {
+        primary: "#005f99",
+        secondary: "#0099cc",
+        lightPrimary: "#4da6ff",
+        darkPrimary: "#003366",
+        lightSecondary: "#66c2ff",
+        darkSecondary: "#00264d",
+        success: "#00b386",
+        danger: "#cc3300",
+        warning: "#e6b800",
+        background: "#e6f2ff"
+    },
+    fontSizes: {
+        extraSmall: "0.8rem",
+        small: "1rem",
+        medium: "1.25rem",
+        extraLarge: "1.8rem"
+    }
+};
+
+export default theme;

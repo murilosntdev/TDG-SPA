36f04ff2c48407805f57d78e8626d5dff636a5eb
murilosntdev <murilosntdev@gmail.com>
2024-12-12 17:29:51 -0300

    getAccountInfo function

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -2,16 +2,33 @@ import { useNavigate } from "react-router-dom";
 import { Footer } from "../../components/Footer/Footer";
 import Navbar from "../../components/Navbar/Navbar";
 import * as S from "./DashboardStyles"
-import api from "../../services/api";
+import { api, authFetch } from "../../services/api";
+import { useEffect } from "react";
 
 const Dashboard = () => {
     const navigate = useNavigate();
 
+    useEffect(() => {
+        const getAccountInfo = async () => {
+            authFetch("/account/infos", { navigate }).then(data => {
+                if (data.status === 200) {
+                    const accountInfos = data.details.account_info;
+
+                    sessionStorage.setItem("accountInfos", JSON.stringify(accountInfos));
+                };
+            });
+        };
+
+        getAccountInfo();
+    }, [navigate]);
+
     async function handleLogout() {
         try {
             await api.post('/session/logout', {}, { withCredentials: true });
+            sessionStorage.clear();
             navigate("/");
         } catch (error) {
+            sessionStorage.clear();
             navigate("/");
         }
     };
